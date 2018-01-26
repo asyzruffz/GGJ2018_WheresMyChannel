1f9b159e32c45b60b892d3c16adda787174baf74
Muhammad Asyraf bin Ibrahim <asyzruffz@gmail.com>
2018-01-27 01:26:17 +0800

    Added code for antenna, signal, etc

---
+++ Assets/Scripts/RadioSignal.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RadioSignal : MonoBehaviour {
+	
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+
+	void OnDrawGizmos () {
+		CircleCollider2D col = GetComponent<CircleCollider2D> ();
+		if (col) {
+			Gizmos.color = Color.yellow;
+			Gizmos.DrawWireSphere (transform.position, col.radius);
+		}
+	}
+}
