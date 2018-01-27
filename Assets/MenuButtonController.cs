12ec6728083b29ee6cd819ea3abd36a7ea8efa86
James <mozziehin@gmail.com>
2018-01-28 04:22:21 +0800

    Finished the menu scene Created build setting for scene transfer

---
+++ Assets/MenuButtonController.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuButtonController : MonoBehaviour {
+
+	public bool isCredit;
+	public bool isPlay;
+	public bool isQuit;
+	public bool isBack;
+	private bool exiting;
+	private bool entering;
+
+	public AudioSource buttonSFX;
+
+	public AudioClip buttonUpSFX;
+	public AudioClip buttonDownSFX;
+
+	public GameObject buttonTop;
+
+	public GameObject menuObject;
+	public GameObject creditObject;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnMouseDown()
+	{
+		exiting = false;
+		entering = true;
+		if (entering) {
+			buttonSFX.PlayOneShot(buttonDownSFX);
+			buttonTop.transform.position = new Vector3 (buttonTop.transform.position.x, buttonTop.transform.position.y - 0.35f, 0);
+		}
+	}
+
+	void OnMouseExit ()
+	{
+		if (entering) {
+			buttonSFX.PlayOneShot(buttonUpSFX);
+			buttonTop.transform.position = new Vector3 (buttonTop.transform.position.x, buttonTop.transform.position.y + 0.35f, 0);
+			entering = false;
+			exiting = true;
+		}
+	}
+
+	void OnMouseUpAsButton()
+	{
+		if (!exiting) {
+			buttonSFX.PlayOneShot (buttonUpSFX);
+			buttonTop.transform.position = new Vector3 (buttonTop.transform.position.x, buttonTop.transform.position.y + 0.35f, 0);
+			entering = false;
+			if (isPlay) {
+				Application.LoadLevel ("Test");
+			}
+			if (isCredit) {
+				menuObject.SetActive (false);
+				creditObject.SetActive (true);
+			}
+			if (isBack) {
+				menuObject.SetActive (true);
+				creditObject.SetActive (false);
+			}
+			if (isQuit) {
+				Application.Quit ();
+			}
+		}
+	}
+}
