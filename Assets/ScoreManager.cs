1f72adf4f6a2048ac6c30e79c2e3cb08e638d297
James <mozziehin@gmail.com>
2018-01-28 13:39:48 +0800

    Added score script and prefab

---
+++ Assets/ScoreManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour {
+
+	public bool gameOver;
+	public float scoreNumber;
+	public int finalScore;
+	public Text scoreCanvas;
+
+	// Use this for initialization
+	void Start () 
+	{
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		finalScore = Mathf.FloorToInt (scoreNumber);
+		scoreCanvas.text = finalScore.ToString ();
+	}
+
+	void FixedUpdate()
+	{
+		if (!gameOver) 
+		{
+			scoreNumber += Time.deltaTime * 2;
+		}
+	}
+}
